93eb0a139502b37c2638fe157b0208fbeebb992b
danilocorcisp <danilocorci@gmail.com>
2021-03-05 17:34:41 +0100

    audio

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
     import Timer from "./Timer.svelte";
     import HowTo from "./HowTo.svelte"
+    let audio;
+    function timerEnds(e) {
+        audio.play();
+
+    }
 </script>
 
 <style>
@@ -16,9 +21,14 @@
 
 <h1>Handwashing App</h1>
 
-<Timer />
+<Timer on:end={timerEnds}/>
 <HowTo />
 <h3>
     <a href="https://www.who.int">Picture Source</a>
      <a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound Source</a>
 </h3>
+
+<audio bind:this={audio} >
+    <track kind="captions">
+    <source src="yeah.wav" />
+</audio>
